4fa5111567695f7368607d884420678a10a6b3bd
EDSMS <2237643217@qq.com>
2020-02-23 23:01:50 +0800

    Front page HTML now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -45,7 +45,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # 界面中又显示了一个文本框可以输入其他待办事项
