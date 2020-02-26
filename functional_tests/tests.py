8220417a973bff83c56dd5037483c63acb97e861
EDSMS <2237643217@qq.com>
2020-02-26 09:54:40 +0800

    Use Bootstrap to impove layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -10,13 +10,13 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = __browser_url
 chrome_options.add_argument(r'--lang=zh-CN')
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
-
         self.browser = webdriver.Chrome(chrome_options=chrome_options)
 
     def tearDown(self):
+        self.browser.refresh()
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
@@ -133,4 +133,15 @@ class NewVisitorTest(LiveServerTestCase):
             inputbox.location['x'] + inputbox.size['width']/2,
             512,
             delta=10
+        )
+
+        # 她新建了一个清单，看到输入框仍完美地居中显示 
+        inputbox.send_keys('testing') 
+        inputbox.send_keys(Keys.ENTER) 
+        self.wait_for_row_in_list_table('1: testing') 
+        inputbox = self.browser.find_element_by_id('id_new_item') 
+        self.assertAlmostEqual( 
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512, 
+            delta=10 
         )
